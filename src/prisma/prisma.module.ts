fd743f9865371c18d32e9948d00f7ab9305c66a5
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-24 18:46:39 +0700

    feat: move prisma module and service to src directory

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 
 @Global()
 @Module({
-  providers: [PrismaService, JwtStrategy],
-  exports: [PrismaService, JwtStrategy],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
