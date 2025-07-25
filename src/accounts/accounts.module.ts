6fdc0ae15324e3c5b0bfa2ea864336d5703e1e1e
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-07-25 19:06:12 +0700

    feat: Add AccountRepository to accounts model

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
-import { AccountRepository } from './accounts.repository';
+import { AccountsRepository } from './accounts.repository';
 
 @Module({
   controllers: [AccountsController],
-  providers: [AccountsService, AccountRepository],
+  providers: [AccountsService, AccountsRepository],
+  exports: [AccountsRepository],
 })
 export class AccountsModule {}
