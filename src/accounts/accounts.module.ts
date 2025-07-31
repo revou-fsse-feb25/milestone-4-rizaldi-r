e411ff8d54e45f87de142551c6ef91b0280a1803
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-01 02:34:18 +0700

    refactor: export AccountService for other modules

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { AccountsRepository } from './accounts.repository';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [AccountsController],
   providers: [AccountsService, AccountsRepository],
-  exports: [AccountsRepository],
+  exports: [AccountsService, AccountsRepository],
 })
 export class AccountsModule {}
