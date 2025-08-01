50c62646a4d8c3e33a73549f9bb89928326eae6c
Rizaldi <64939575+Rizaldi-R@users.noreply.github.com>
2025-08-01 15:08:41 +0700

    refactor: separate DTOs for requests and responses, add Swagger properties

--- src/user/dto/req/update-user.dto.ts
+++ src/user/dto/req/update-user.dto.ts
@@ -1,22 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsOptional } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsOptional()
   @IsString()
   username?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   firstName?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   lastName?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsEmail()
   email?: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   password?: string;
